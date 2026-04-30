a1dce4e5c20b975f767b7748e44293f7779e17a9
Ridham Patel <Ridhamsangani.23@gmail.com>
2026-04-30 15:16:10 -0700

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    //Sliding window with a frequency map
+    public int characterReplacement(String s, int k) {
+        if (s.length() <= k) return s.length();
+        int maxLen = -1;
+        Map<Character, Integer> freq = new HashMap<>();
+        int maxf = 0;
+        int l = 0;
+        for (int r = l; r < s.length(); r++){
+            freq.put(s.charAt(r), freq.getOrDefault(s.charAt(r), 0) + 1);
+
+            for (int frequency : freq.values()){
+                maxf = Math.max(maxf, frequency);
+            }
+
+            while ((r-l+1) - maxf > k){
+                freq.put(s.charAt(l), freq.get(s.charAt(l)) - 1);
+                l++;
+            }
+
+            maxLen = Math.max(maxLen, r-l+1);
+        }
+
+      
+        return maxLen;
+        
+    }
+}
